fc1032058e0a0e60b7b231259b86228220086117
Heather Barr <hnbarr@yahoo.com>
2019-05-08 00:35:35 -0500

    top middle done

--- src/MiddleComponent/index.js
+++ src/MiddleComponent/index.js
@@ -6,14 +6,14 @@ export default class MiddleComponent extends Component {
     return (
       <div className='middle'>
         <div className='middle-top'>
-        <i className="far fa-grin-tongue fa-3x"></i>
-        <form id='tweet'>
-            <input type='text' placeholder="What's happening?"></input>
-            <button id='addPhoto' type='submit'>
-                <i className="fas fa-camera fa-2x"></i>
-            </button>
-        </form>
-      </div>
+            <i className="far fa-grin-tongue fa-3x"></i>
+            <form id='tweet'>
+                <input id='textBox' type='text' placeholder="What's happening?"></input>
+                <button id='addPhoto' type='submit'>
+                    <i className="fas fa-camera fa-2x"></i>
+                </button>
+            </form> 
+        </div>
     
         <div className='middle-bottom'>
         <p>Twitter Feed</p>
