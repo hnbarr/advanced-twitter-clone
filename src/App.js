afc8b046028e7a6a5970d4375b125c5a19cc1162
Heather Barr <hnbarr@yahoo.com>
2019-05-07 20:13:41 -0500

    outline complete bitchessss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavComponent from './NavComponent';
+import LeftComponent from './LeftComponent'
+import MiddleComponent from './MiddleComponent'
+import RightComponent from './RightComponent' 
 
 function App(){
    return (
-    <div>
-
+    <div className='App'>
+    <NavComponent />
+      <div className='components'>
+      <LeftComponent />
+      <MiddleComponent />
+      <RightComponent />
+      </div>
     </div>
   );
  }
